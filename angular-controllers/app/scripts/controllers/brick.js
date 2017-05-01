6906ffc14f934ff8c594a58ee23b390ede6e7fb4
tlabna <tlabna@users.noreply.github.com>
2017-05-01 15:20:00 -0400

    feat: add brick controller

---
+++ angular-controllers/app/scripts/controllers/brick.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('controllerQuizApp', [])
+    .controller('BrickCtrl', function() {
+        this.color = 'Green';
+        this.size = '2 x 6';
+        this.price = 0.03;
+        this.quantity = 697;
+    });
