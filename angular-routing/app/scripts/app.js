50374af5506e2065d70af7bcd8c1c798d64b2cee
tlabna <tlabna@users.noreply.github.com>
2017-05-02 08:50:04 -0400

    feat: add ui-routing with bricks displaying on signle page with nested routing

--- angular-routing/app/scripts/app.js
+++ angular-routing/app/scripts/app.js
@@ -9,4 +9,52 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', []);
+    .module('routingQuizApp', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url : '/',
+                templateUrl: 'views/instructions.html'
+            })
+
+            .state('redBrick', {
+                url : '/bricks/red',
+                templateUrl: 'views/bricks.html',
+                controller: 'RedBricksCtrl as brick'
+            })
+            .state('redBrick.cart', {
+                url : '/bricks/red/cart',
+                templateUrl: 'views/cart.html',
+                controller: 'CartCtrl as cart'
+            })
+
+            .state('blueBrick', {
+                url : '/bricks/blue',
+                templateUrl: 'views/bricks.html',
+                controller: 'BlueBricksCtrl as brick'
+            })
+            .state('blueBrick.cart', {
+                url : '/bricks/blue/cart',
+                templateUrl: 'views/cart.html',
+                controller: 'CartCtrl as cart'
+            })
+
+            .state('greenBrick', {
+                url : '/bricks/green',
+                templateUrl: 'views/bricks.html',
+                controller: 'GreenBricksCtrl as brick'
+            })
+            .state('greenBrick.cart', {
+                url : '/bricks/green/cart',
+                templateUrl: 'views/cart.html',
+                controller: 'CartCtrl as cart'
+            })
+
+            .state('cart', {
+                url : '/cart',
+                templateUrl: 'views/cart.html',
+                controller: 'CartCtrl as cart'
+            });
+    }]);
